6219f589d70012b63d5abf76e25928ce5c151fa5
varaprasad9392 <97425886+varaprasad9392@users.noreply.github.com>
2022-05-17 21:34:11 +0530

    a=int(input()) str=['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'] for i in range(a):     for j in range(a):         print(str[i],end=' ')     print('')

---
+++ print_the_following_pattern.py
@@ -0,0 +1,6 @@
+a=int(input())
+str=['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+for i in range(a):
+    for j in range(a):
+        print(str[i],end=' ')
+    print('')
