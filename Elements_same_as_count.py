d557fdd8778fe6456c657affe37e2b447f6613bb
varaprasad9392 <97425886+varaprasad9392@users.noreply.github.com>
2022-09-20 10:39:25 +0530

    a=int(input()) l=list(map(int,input().split())) count=0 su=0 x=[] l1=set(l) l2=list(l1) for i in range(len(l2)):     count=0     for j in range(len(l)):         if l2[i]==l[j]:             count+=1     if count==l2[i]:         x.append(l[i])         su+=1 if su==0:     print('-1') else:     print(*x)

---
+++ Elements_same_as_count.py
@@ -0,0 +1,19 @@
+a=int(input())
+l=list(map(int,input().split()))
+count=0
+su=0
+x=[]
+l1=set(l)
+l2=list(l1)
+for i in range(len(l2)):
+    count=0
+    for j in range(len(l)):
+        if l2[i]==l[j]:
+            count+=1
+    if count==l2[i]:
+        x.append(l[i])
+        su+=1
+if su==0:
+    print('-1')
+else:
+    print(*x)
