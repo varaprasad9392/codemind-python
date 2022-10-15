6907b823041f8c7a76b6f31597274ea904113d84
varaprasad9392 <97425886+varaprasad9392@users.noreply.github.com>
2022-10-16 00:47:59 +0530

    x,y=map(int,input().split()) l=list(map(int,input().split())) m = list(map(int,input().split())) for i in sorted(set(l),key=l.index):     if m.count(i)==0:         print(i,end=" ") for j in sorted(set(m),key=m.index):     if l.count(j)==0:         print(j,end=" ")

--- Common_elements_II.py
+++ Common_elements_II.py
@@ -1,20 +1,9 @@
-a,b=map(int,input().split())
-l1=list(map(int,input().split()))
-l2=list(map(int,input().split()))
-x=[]
-for i in range(len(l1)):
-    count=0
-    for j in range(len(l1)):
-        if l1[i]==l1[j]:
-            count+=1
-    if count==1 and l1[i] not in l2:
-        x.append(l1[i])
-count=0
-for i in range(len(l2)):
-    count=0
-    for j in range(len(l2)):
-        if l2[i]==l2[j]:
-            count+=1
-    if count==1 and l2[i] not in l1:
-        x.append(l2[i])
-print(*x)
+x,y=map(int,input().split())
+l=list(map(int,input().split()))
+m = list(map(int,input().split()))
+for i in sorted(set(l),key=l.index):
+    if m.count(i)==0:
+        print(i,end=" ")
+for j in sorted(set(m),key=m.index):
+    if l.count(j)==0:
+        print(j,end=" ")
